83dc59790ddc9b6e14110c764af478313ea20046
Martin Kolárik <martin@kolarik.sk>
2018-06-02 19:38:31 +0200

    Allow logging Origin/Referrer headers as a custom tag

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ const ElasticWriter = require('./ElasticWriter');
 const libs = require('./libs');
 
 module.exports = ElasticWriter;
+module.exports.express = libs.express;
 module.exports.koa = libs.koa;
--- src/libs.js
+++ src/libs.js
@@ -1,15 +1,53 @@
+const url = require('fast-url-parser');
+
+module.exports.express = {
+	middleware (apmClient, { setOrigin = true } = {}) {
+		if (!apmClient) {
+			return (req, res, next) => next();
+		}
+
+		return (req, res, next) => {
+			if (setOrigin) {
+				let origin = req.get('origin') || req.get('referrer');
+
+				if (origin) {
+					let parsed = url.parse(origin);
+					apmClient.setTag('origin', `${parsed.protocol}//${parsed.host}`);
+				}
+			}
+
+			return next();
+		};
+	},
+};
+
 module.exports.koa = {
 	addRoutes (router, routes, fn) {
 		routes.forEach((route) => {
 			router.get(route[0], route[1], fn);
 		});
 	},
-	middleware (apmClient, prefix = '') {
-		return async (ctx, next) => {
-			let matched = ctx.matched.find(r => r.name);
+	middleware (apmClient, { prefix = '', setNames = true, setOrigin = true } = {}) {
+		if (!apmClient) {
+			return async (ctx, next) => next();
+		}
 
-			if (matched && apmClient) {
-				apmClient.setTransactionName(`${ctx.request.method} ${prefix}${matched.name}`);
+		return async (ctx, next) => {
+			if (setNames) {
+				let matched = ctx.matched.find(r => r.name);
+
+				if (matched) {
+					apmClient.setTransactionName(`${ctx.request.method} ${prefix}${matched.name}`);
+				}
+			}
+
+			if (setOrigin) {
+				let origin = ctx.request.get('origin') || ctx.request.get('referrer');
+
+				if (origin) {
+					let parsed = url.parse(origin);
+					apmClient.setTag('origin', `${parsed.protocol}//${parsed.host}`);
+				}
 			}
 
 			return next();
