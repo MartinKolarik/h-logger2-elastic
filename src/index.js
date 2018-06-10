7105a98b861d2bf50a4df283f9e5cf355c11b1b9
Martin Kolárik <martin@kolarik.sk>
2018-06-10 19:44:06 +0200

    Move utils to a separate module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 const ElasticWriter = require('./ElasticWriter');
-const libs = require('./libs');
-
 module.exports = ElasticWriter;
-module.exports.express = libs.express;
-module.exports.koa = libs.koa;
