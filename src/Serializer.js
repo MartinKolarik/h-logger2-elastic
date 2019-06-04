2b8cb1f29b173a314567c41f2b506d17612cf2d6
Martin Kolárik <martin@kolarik.sk>
2019-06-05 00:50:08 +0200

    Truncate long strings

--- src/Serializer.js
+++ src/Serializer.js
@@ -3,6 +3,12 @@ const stringify = require('safe-json-stringify');
 
 module.exports = class extends Serializer {
 	serialize (object) {
-		return stringify(object);
+		return stringify(object, (name, value) => {
+			if (typeof value === 'string' && value.length > 4096) {
+				return value.slice(0, 2048) + `\n... truncated ${value.length - 4096} bytes ...\n` + value.slice(-2048);
+			}
+
+			return value;
+		});
 	}
 };
